77a55182452e43773001e7f2095dab7a026a32eb
madelinekreiff <madelinekreiff@gmail.com>
2021-09-07 16:14:39 -0700

    Edited div and divParent styles in changeText function

--- js/about.js
+++ js/about.js
@@ -1,10 +1,24 @@
 // variable for the about-para class that holds the about-sq articles & about-hover divs
 const aboutPara = document.querySelector(".about-para");
 
+const changeText = id => {
+  const div = document.querySelector(`#${id}`);
+  const divParent = div.parentElement;
+
+  divParent.style.backgroundColor = "#506f7a";
+  divParent.style.border = "3px solid #bab4a4";
+  divParent.style.opacity = "0.5";
+  div.style.fontFamily = "'PT Serif', serif";
+  div.style.fontSize = "0.3em";
+  div.style.opacity = "1";
+
+}; // end changeText
+
+
 // event listener for clicking the about-hover divs inside aboutPara
 aboutPara.addEventListener("click", function(e) {
   if (e.target.classList.contains("about-hover")) {
     const id = e.target.id;
-    console.log(id);
+    changeText(id);
   }
 }); // end event listener
